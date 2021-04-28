9ba2087861f6b3c28dff6ec5b9b9e3696a97261f
Hasan Alsharif <hasanxalsharif@gmail.com>
2021-04-28 02:44:51 +0200

    main page elements

--- expensesTracker.js
+++ expensesTracker.js
@@ -1,2 +1,18 @@
-const h1 = $("#h1");
-h1.text("Welcome")
+const body = $("body");
+
+const maindiv = $("<div id='maindiv'> </div>")
+
+
+const mainh = $("<h1 id= 'mainh' >Welcome to MyExpenses Tracker</h1>")
+
+
+
+const moto= $("<p id='moto'> Your money's best friend </p>")
+
+
+
+maindiv.appendTo(body)
+mainh.appendTo(maindiv)
+moto.appendTo(maindiv)
+
+
